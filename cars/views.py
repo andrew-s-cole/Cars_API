55186759b4e262156d80ef8892d09a3218799117
andrew-s-cole <andrew.s.cole@gmail.com>
2022-04-12 18:20:09 -0400

    Updated framework to add new requests in Postman

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import CarSerializer
+from .models import Car
+
+
+@api_view(['GET'])
+def cars_list(request):
+    cars = Car.objects.all()
+
+    serializer = CarSerializer(cars, many=True)
+
+    return Response(serializer.data)
